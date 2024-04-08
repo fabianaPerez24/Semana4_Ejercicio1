c8b9cb087c2d39999bd99096660d6d7904f44654
fabianaPerez24 <fabianalertora003@gmail.com>
2024-04-08 17:35:24 -0500

    primer commit, se creo la clase menu

---
+++ Semana4_actividad1/Menu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semana4_actividad1
+{
+    internal class Menu
+    {
+        public void SetANumber()
+        {
+            Console.WriteLine("Inserta un numero");
+            int n = int.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine("El fibonacci del numero es:");
+            Console.WriteLine("El primo del numero es:");
+            Console.WriteLine("Su numero perfecto es");
+        }
+
+
+        public void Fibonacci()
+        {
+            int a = 0;
+            int b = 1;
+
+            int c;
+
+            for (int i = 0; i < n; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+        }
+        public void Primos()
+        {
+
+        }
+        public void NumeroPerfecto()
+        {
+
+        }
+    }
+}
